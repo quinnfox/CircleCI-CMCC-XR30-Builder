eeed1a47357795d829d349c959e77622b4cf01a3
Git IY! <91715524+gitiy1@users.noreply.github.com>
2025-08-30 12:22:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -101,84 +101,84 @@ config_package_del luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client
 config_package_del luci-app-ssr-plus_INCLUDE_ShadowsocksR_NONE_Server
 config_package_del luci-theme-bootstrap-mod
 
-# Add custom packages
+# # Add custom packages
 
 ## Web Terminal
 config_package_add luci-app-ttyd
-## IP-Mac Binding
-config_package_add luci-app-arpbind
-## Wake on Lan
-config_package_add luci-app-wol
-## QR Code Generator
-config_package_add qrencode
-## Zsh
-config_package_add zsh
-## Temporarily disable USB3.0
-config_package_add luci-app-usb3disable
-## USB
-# config_package_add kmod-usb-net-huawei-cdc-ncm
-config_package_add kmod-usb-net-ipheth
-config_package_add kmod-usb-net-aqc111
-config_package_add kmod-usb-net-rtl8152-vendor
-config_package_add kmod-usb-net-sierrawireless
-config_package_add kmod-usb-storage
-config_package_add kmod-usb-ohci
-config_package_add kmod-usb-uhci
-config_package_add usb-modeswitch
-config_package_add sendat
+# ## IP-Mac Binding
+# config_package_add luci-app-arpbind
+# ## Wake on Lan
+# config_package_add luci-app-wol
+# ## QR Code Generator
+# config_package_add qrencode
+# ## Zsh
+# config_package_add zsh
+# ## Temporarily disable USB3.0
+# config_package_add luci-app-usb3disable
+# ## USB
+# # config_package_add kmod-usb-net-huawei-cdc-ncm
+# config_package_add kmod-usb-net-ipheth
+# config_package_add kmod-usb-net-aqc111
+# config_package_add kmod-usb-net-rtl8152-vendor
+# config_package_add kmod-usb-net-sierrawireless
+# config_package_add kmod-usb-storage
+# config_package_add kmod-usb-ohci
+# config_package_add kmod-usb-uhci
+# config_package_add usb-modeswitch
+# config_package_add sendat
 ## bbr
 config_package_add kmod-tcp-bbr
-## coremark cpu 跑分
-config_package_add coremark
-## autocore + lm-sensors-detect： cpu 频率、温度
-config_package_add autocore
-config_package_add lm-sensors-detect
-## autoreboot
-config_package_add luci-app-autoreboot
-## 多拨
-config_package_add kmod-macvlan
-config_package_add mwan3
-config_package_add luci-app-mwan3
-# ## frpc
-# config_package_add luci-app-frpc
-## mosdns
-config_package_add luci-app-mosdns
+# ## coremark cpu 跑分
+# config_package_add coremark
+# ## autocore + lm-sensors-detect： cpu 频率、温度
+# config_package_add autocore
+# config_package_add lm-sensors-detect
+# ## autoreboot
+# config_package_add luci-app-autoreboot
+# ## 多拨
+# config_package_add kmod-macvlan
+# config_package_add mwan3
+# config_package_add luci-app-mwan3
+# # ## frpc
+# # config_package_add luci-app-frpc
+# ## mosdns
+# config_package_add luci-app-mosdns
 ## curl
 config_package_add curl
 ## netcat
 config_package_add netcat
-## disk
-# config_package_add gdisk
-# config_package_add sgdisk
+# ## disk
+# # config_package_add gdisk
+# # config_package_add sgdisk
 
 
-# Third-party packages
-mkdir -p package/custom
-git clone --depth 1  https://github.com/217heidai/OpenWrt-Packages.git package/custom
-clean_packages package/custom
+# # Third-party packages
+# mkdir -p package/custom
+# git clone --depth 1  https://github.com/217heidai/OpenWrt-Packages.git package/custom
+# clean_packages package/custom
 
-## golang
-rm -rf feeds/packages/lang/golang
-mv package/custom/golang feeds/packages/lang/
+# ## golang
+# rm -rf feeds/packages/lang/golang
+# mv package/custom/golang feeds/packages/lang/
 
-## Passwall
-config_package_add luci-app-passwall
-config_package_add luci-app-passwall_Nftables_Transparent_Proxy
-config_package_del luci-app-passwall_Iptables_Transparent_Proxy
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
-config_package_del luci-app-passwall_INCLUDE_Trojan_Plus
-config_package_del luci-app-passwall_INCLUDE_Simple_Obfs
-config_package_del luci-app-passwall_INCLUDE_tuic_client
+# ## Passwall
+# config_package_add luci-app-passwall
+# config_package_add luci-app-passwall_Nftables_Transparent_Proxy
+# config_package_del luci-app-passwall_Iptables_Transparent_Proxy
+# config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client
+# config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server
+# config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client
+# config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server
+# config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client
+# config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
+# config_package_del luci-app-passwall_INCLUDE_Trojan_Plus
+# config_package_del luci-app-passwall_INCLUDE_Simple_Obfs
+# config_package_del luci-app-passwall_INCLUDE_tuic_client
 
 
-## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
-config_package_add luci-app-autotimeset
-config_package_add luci-lib-ipkg
+# ## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
+# config_package_add luci-app-autotimeset
+# config_package_add luci-lib-ipkg
 
 ## byobu, tmux
 # config_package_add byobu
