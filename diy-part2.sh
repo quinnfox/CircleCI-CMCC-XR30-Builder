923ad65a9dee274a130cae7392937461aaf52ce0
Git IY! <91715524+gitiy1@users.noreply.github.com>
2025-08-30 22:13:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -113,115 +113,113 @@ config_package_del luci-app-ssr-plus_INCLUDE_NONE_V2RAY
 config_package_del luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client
 config_package_del luci-app-ssr-plus_INCLUDE_ShadowsocksR_NONE_Server
 config_package_del luci-theme-bootstrap-mod
-
-# # Add custom packages
+config_package_del luci-app-ssr-plus_INCLUDE_ShadowsocksR_Rust_Client
+config_package_del luci-app-ssr-plus_INCLUDE_ShadowsocksR_Rust_Server
+# Add custom packages
 
 ## Web Terminal
 config_package_add luci-app-ttyd
-# ## IP-Mac Binding
-# config_package_add luci-app-arpbind
-# ## Wake on Lan
-# config_package_add luci-app-wol
-# ## QR Code Generator
-# config_package_add qrencode
-# ## Zsh
-# config_package_add zsh
-# ## Temporarily disable USB3.0
-# config_package_add luci-app-usb3disable
-# ## USB
-# # config_package_add kmod-usb-net-huawei-cdc-ncm
-# config_package_add kmod-usb-net-ipheth
-# config_package_add kmod-usb-net-aqc111
-# config_package_add kmod-usb-net-rtl8152-vendor
-# config_package_add kmod-usb-net-sierrawireless
-# config_package_add kmod-usb-storage
-# config_package_add kmod-usb-ohci
-# config_package_add kmod-usb-uhci
-# config_package_add usb-modeswitch
-# config_package_add sendat
+## IP-Mac Binding
+config_package_add luci-app-arpbind
+## Wake on Lan
+config_package_add luci-app-wol
+## QR Code Generator
+config_package_add qrencode
+## Fish
+config_package_add fish
+## Temporarily disable USB3.0
+config_package_add luci-app-usb3disable
+## USB
+# config_package_add kmod-usb-net-huawei-cdc-ncm
+config_package_add kmod-usb-net-ipheth
+config_package_add kmod-usb-net-aqc111
+config_package_add kmod-usb-net-rtl8152-vendor
+config_package_add kmod-usb-net-sierrawireless
+config_package_add kmod-usb-storage
+config_package_add kmod-usb-ohci
+config_package_add kmod-usb-uhci
+config_package_add usb-modeswitch
+config_package_add sendat
 ## bbr
 config_package_add kmod-tcp-bbr
-# ## coremark cpu 跑分
+## coremark cpu 跑分
 # config_package_add coremark
-# ## autocore + lm-sensors-detect： cpu 频率、温度
+## autocore + lm-sensors-detect： cpu 频率、温度
 # config_package_add autocore
 # config_package_add lm-sensors-detect
-# ## autoreboot
-# config_package_add luci-app-autoreboot
-# ## 多拨
-# config_package_add kmod-macvlan
-# config_package_add mwan3
-# config_package_add luci-app-mwan3
-# # ## frpc
-# # config_package_add luci-app-frpc
-# ## mosdns
+## autoreboot
+config_package_add luci-app-autoreboot
+## 多拨
+config_package_add kmod-macvlan
+config_package_add mwan3
+config_package_add luci-app-mwan3
+# ## frpc
+# config_package_add luci-app-frpc
+## mosdns
 # config_package_add luci-app-mosdns
 ## curl
 config_package_add curl
-## netcat
-config_package_add netcat
-# ## disk
-# # config_package_add gdisk
-# # config_package_add sgdisk
+## socat
+config_package_add socat
+## disk
+config_package_add gdisk
+config_package_add sgdisk
+## Vim-Full
+config_package_add vim-full
+## iperf
+config_package_add iperf
 
+# MentoHust
+git clone https://github.com/sbwml/luci-app-mentohust package/mentohust
+config_package_add luci-app-mentohust
 
-# # Third-party packages
-# mkdir -p package/custom
-# git clone --depth 1  https://github.com/217heidai/OpenWrt-Packages.git package/custom
-# clean_packages package/custom
+# Daed
+git clone https://github.com/QiuSimons/luci-app-daed package/daed
+config_package_add luci-app-daed
 
-# ## golang
-# rm -rf feeds/packages/lang/golang
-# mv package/custom/golang feeds/packages/lang/
+# Third-party packages
+mkdir -p package/custom
+git clone --depth 1  https://github.com/217heidai/OpenWrt-Packages.git package/custom
+clean_packages package/custom
 
-# ## Passwall
-# config_package_add luci-app-passwall
-# config_package_add luci-app-passwall_Nftables_Transparent_Proxy
-# config_package_del luci-app-passwall_Iptables_Transparent_Proxy
-# config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client
-# config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server
-# config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client
-# config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server
-# config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client
-# config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
-# config_package_del luci-app-passwall_INCLUDE_Trojan_Plus
-# config_package_del luci-app-passwall_INCLUDE_Simple_Obfs
-# config_package_del luci-app-passwall_INCLUDE_tuic_client
+## golang
+rm -rf feeds/packages/lang/golang
+mv package/custom/golang feeds/packages/lang/
 
+## Passwall
+# config_package_add luci-app-passwall2
+# config_package_add iptables-mod-socket
+# config_package_add luci-app-passwall2_Iptables_Transparent_Proxy
+# config_package_add luci-app-passwall2_INCLUDE_Hysteria
+# config_package_del luci-app-passwall2_Nftables_Transparent_Proxy
+# config_package_del luci-app-passwall2_INCLUDE_Shadowsocks_Libev_Client
+# config_package_del luci-app-passwall2_INCLUDE_Shadowsocks_Libev_Server
+# config_package_del luci-app-passwall2_INCLUDE_Shadowsocks_Rust_Client
+# config_package_del luci-app-passwall2_INCLUDE_Shadowsocks_Rust_Server
+# config_package_del luci-app-passwall2_INCLUDE_ShadowsocksR_Libev_Client
+# config_package_del luci-app-passwall2_INCLUDE_ShadowsocksR_Libev_Server
+# config_package_del luci-app-passwall2_INCLUDE_Trojan_Plus
+# config_package_del luci-app-passwall2_INCLUDE_Simple_Obfs
+# config_package_del luci-app-passwall2_INCLUDE_tuic_client
+# config_package_del shadowsocks-libev-config
+# config_package_del shadowsocks-libev-ss-local
+# config_package_del shadowsocks-libev-ss-redir
+# config_package_del shadowsocks-libev-ss-server
+# config_package_del shadowsocksr-libev-ssr-local
+# config_package_del shadowsocksr-libev-ssr-redir
+# config_package_del shadowsocks-libev-ssr-server
+# config_package_del shadowsocks-rust
+# config_package_del simple-obfs
+rm -r package/custom/shadowsocks-rust
+rm -r package/custom/simple-obfs
 
-# ## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
-# config_package_add luci-app-autotimeset
-# config_package_add luci-lib-ipkg
+## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
+config_package_add luci-app-autotimeset
+config_package_add luci-lib-ipkg
 
 ## byobu, tmux
-# config_package_add byobu
-#config_package_add tmux
-
-# ## Frp Latest version patch
-
-# FRP_MAKEFILE_PATH="feeds/packages/net/frp/Makefile"
-
-# FRP_LATEST_RELEASE=$(curl -s https://api.github.com/repos/fatedier/frp/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-
-# if [ -z "$FRP_LATEST_RELEASE" ]; then
-  # echo "无法获取最新的 Release 名称"
-  # exit 1
-# fi
-
-# FRP_LATEST_VERSION=${FRP_LATEST_RELEASE#v}
-
-# FRP_PKG_NAME="frp"
-# FRP_PKG_SOURCE="${FRP_PKG_NAME}-${FRP_LATEST_VERSION}.tar.gz"
-# FRP_PKG_SOURCE_URL="https://codeload.github.com/fatedier/frp/tar.gz/v${FRP_LATEST_VERSION}?"
-# curl -L -o "$FRP_PKG_SOURCE" "$FRP_PKG_SOURCE_URL"
-
-# FRP_PKG_HASH=$(sha256sum "$FRP_PKG_SOURCE" | awk '{print $1}')
-# rm -r "$FRP_PKG_SOURCE"
-
-# sed -i "s/^PKG_VERSION:=.*/PKG_VERSION:=${FRP_LATEST_VERSION}/" "$FRP_MAKEFILE_PATH"
-# sed -i "s/^PKG_HASH:=.*/PKG_HASH:=${FRP_PKG_HASH}/" "$FRP_MAKEFILE_PATH"
-
-# echo "已更新 Makefile 中的 PKG_VERSION 和 PKG_HASH"
+config_package_add byobu
+config_package_add tmux
 
 # Change default LAN IP to 192.168.3.1
 echo "🌐 Setting LAN IP to 192.168.3.1"
